74641507221bac2c4cca4627693cf9c9803d1ad6
BEL gauna <145999980+Belconwes@users.noreply.github.com>
2024-06-05 21:45:26 -0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,19 @@
+Create database Abmclientes;
+Use Abmclientes;
+
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY identity(1,1) ,
+    Nombre NVARCHAR(50),
+    Apellido NVARCHAR(50),
+    Email NVARCHAR(100),
+    Telefono NVARCHAR(15),
+    Direccion NVARCHAR(100),
+    Ciudad NVARCHAR(50),
+    Pais NVARCHAR(50),
+    CodigoPostal NVARCHAR(10),
+    FechaRegistro DATE
+);
+
+
+select * from Clientes
+
